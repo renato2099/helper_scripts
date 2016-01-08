8869694ed62cba8c68eb7aa46c86c05547348169
Markus Pilman <mpilman@inf.ethz.ch>
2016-01-08 14:49:56 +0100

    fixed tellstore bug

--- tpch_population.py
+++ tpch_population.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 import os
 
 from ServerConfig import Client
+from ServerConfig import General
 from ServerConfig import Storage
 from ServerConfig import Kudu
 from ServerConfig import Tpch
@@ -10,7 +11,7 @@ from ServerConfig import TellStore
 
 storageAddr = Kudu.master + " -k"
 if Storage.storage == TellStore:
-    storageAddr = '"{0}"'.format(reduce(lambda x, y: x + ";" + y, TellStore.servers))
+    storageAddr = '"{0}"'.format(reduce(lambda x, y: x + ";" + y, map(lambda x: General.infinibandIp[x] ,TellStore.servers)))
 
 cmd = "{0}/watch/tpcc/tpch -S {1} -d {2}".format(TellStore.builddir, storageAddr, Tpch.dbgenFiles)
-print cmd
+exit(os.system(cmd))
