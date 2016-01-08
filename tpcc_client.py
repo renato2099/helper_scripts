7b0f98b33fe5f919f0385801b7c9f5bfd3efe6f5
Markus Pilman <mpilman@inf.ethz.ch>
2016-01-08 13:25:23 +0100

    added -a flag to tpcc_client

--- tpcc_client.py
+++ tpcc_client.py
@@ -7,6 +7,7 @@ from ServerConfig import Tpcc
 from ServerConfig import TellStore
 
 parser = ArgumentParser()
+parser.add_argument("-a", dest="ch", help="Populate for CH-Benchmark" action="store_true")
 parser.add_argument("-P", dest='populate', help="Populate data", action='store_true')
 parser.add_argument("outfile", help="CSV file for results", default="out.csv", nargs='?')
 args = parser.parse_args()
@@ -20,7 +21,10 @@ def addPort(x):
 cmd = '{0}/watch/tpcc/tpcc_client -H "{1}" -c {2} -W {3}'.format(TellStore.builddir, reduce(reduceComma, Tpcc.servers0 + map(addPort, Tpcc.servers1)), Client.numClients, Tpcc.warehouses) 
 
 if (args.populate):
-    cmd = cmd + " -P"
+    if args.ch:
+        cmd = cmd + " -P -a"
+    else:
+        cmd = cmd + " -P"
 else:
     cmd = cmd + " -t {0} -o {1} -l {2}".format(Client.runTime, args.outfile, Client.logLevel)
 
