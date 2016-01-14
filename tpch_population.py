bbd416b8e609dfbe952fca1d5cc0595100e6ebad
braunl <braunl@inf.ethz.ch>
2016-01-14 14:31:34 +0100

    fixed tpch population

--- tpch_population.py
+++ tpch_population.py
@@ -11,7 +11,10 @@ from ServerConfig import TellStore
 
 storageAddr = Kudu.master + " -k"
 if Storage.storage == TellStore:
-    storageAddr = '"{0}"'.format(reduce(lambda x, y: x + ";" + y, map(lambda x: General.infinibandIp[x] ,TellStore.servers)))
+    storageAddr = '"{0}"'.format(reduce(lambda x, y: x + ";" + y, map(lambda x: General.infinibandIp[x] + ":7241" ,TellStore.servers)))
+    masterAddr = General.infinibandIp[TellStore.commitmanager] + ":7242"
 
-cmd = "{0}/watch/tpcc/tpch -S {1} -d {2}".format(TellStore.builddir, storageAddr, Tpch.dbgenFiles)
+cmd = '{0}/watch/tpcc/tpch -S {1} -d {2} -C "{3}"'.format(TellStore.builddir, storageAddr, Tpch.dbgenFiles, masterAddr)
+print cmd
 exit(os.system(cmd))
+
