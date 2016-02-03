ae4b0b6d026c326f448a53cb0e13d16bab1ebf08
Renato Marroquin <renatoj.marroquin@gmail.com>
2016-02-03 12:49:08 +0100

    Cleaning up

--- stop_hdfs.py
+++ stop_hdfs.py
@@ -11,15 +11,6 @@ execssh([Hadoop.namenode], '{0}/sbin/stop-dfs.sh'.format(Hadoop.hadoopdir))
 execssh(Hadoop.datanodes + [Hadoop.namenode], "umount {0}".format(Hadoop.datadir))
 execssh(Hadoop.datanodes + [Hadoop.namenode], "rm -r {0}".format(Hadoop.datadir))
 
-#hadoop-end.sh
-hadoopEnv = '{0}/etc/hadoop/hadoop-env.sh'.format(Hadoop.hadoopdir)
-hadoopEnvFile = open(hadoopEnv)
-lines = hadoopEnvFile.readlines()
-hadoopEnvFile.close()
-w = open(hadoopEnv,'w')
-w.writelines([item for item in lines[:-4]])
-w.close()
-
 stopHdfsCmd = '{0}/sbin/stop-dfs.sh'.format(Hadoop.hadoopdir)
 os.system('ssh root@{0} {1}'.format(Hadoop.namenode, stopHdfsCmd))
 #hdfsClients = ThreadedClients(Hadoop.datanodes, stopHdfsCmd, root=True)
