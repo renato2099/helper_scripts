40eb715663084dead3c8a9d4f086e59cbd4c27c0
braunl <braunl@inf.ethz.ch>
2016-02-26 16:43:32 +0100

    fixed tpch_client script for population

--- tpch_client.py
+++ tpch_client.py
@@ -4,6 +4,7 @@ from threading import Thread
 import time
 import os
 
+from ServerConfig import TellStore
 from ServerConfig import Client
 from ServerConfig import Tpch
 from ServerConfig import TpchWorkload
@@ -13,9 +14,11 @@ parser.add_argument("-P", dest='populate', help="Populate data", action="store_t
 parser.add_argument("outfile", help="CSV file for results", default="out.csv", nargs='?')
 args = parser.parse_args()
 
-cmd = "{0}/watch/tpch/tpch_client -H '{1}' -c {2} -l {3} -d {4} -o {5}".format(TellStore.builddir, Tpch.getServerList(), Client.numClients, Client.logLevel, TpchWorkload.dbgenFiles, args.outfile) 
+cmd = "{0}/watch/tpch/tpch_client -H '{1}' -c {2} -l {3} -o {4} -d ".format(TellStore.builddir, Tpch.getServerList(), Client.numClients, Client.logLevel, args.outfile) 
 if (args.populate):
-    cmd = cmd + ' -P'
+    cmd = cmd + '{0} -P'.format(TpchWorkload.dbgenFiles)
+else:
+    cmd = cmd + '{0}'.format(TpchWorkload.updateFiles)
 
 print "Execute {0}".format(cmd)
 exit(os.system(cmd))
