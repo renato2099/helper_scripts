039d9e753d59ac150d261bdfb973afcd70473a9f
Renato Marroquin <renatoj.marroquin@gmail.com>
2016-04-08 14:46:21 +0200

    Adding master service to stop it

--- stop_java_unmount_memfs.py
+++ stop_java_unmount_memfs.py
@@ -7,6 +7,7 @@ from ServerConfig import General
 from ServerConfig import Storage
 from ServerConfig import Cassandra
 from ServerConfig import Hadoop
+from ServerConfig import Hbase
 
 def execssh(hosts, cmd):
     clients = ThreadedClients(hosts, cmd, root=True)
@@ -17,7 +18,7 @@ def stop_java_unmount_memfs():
     if Storage.storage == Cassandra:
         execssh(Storage.servers, "umount {0}; rm -r {0}; rm -r {1}".format(Cassandra.datadir, Cassandra.logdir))
     elif Storage.storage == Hbase or Storage.storage == Hadoop:
-        execssh(Storage.servers, "umount {0}; rm -r {0}".format(Hadoop.datadir)) 
+        execssh([Storage.master] + Storage.servers, "umount {0}; rm -r {0}".format(Hadoop.datadir)) 
 
 if __name__ == '__main__':
     stop_java_unmount_memfs()
